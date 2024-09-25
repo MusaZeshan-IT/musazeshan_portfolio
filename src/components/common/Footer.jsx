7c4f821be3e806434dc96214f27e8d15b37482ec
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-25 20:04:11 +0500

    Now the footer is also been made responsive.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='bg-[rgb(26,22,66)] py-10'>
+        <footer className='bg-[rgb(26,22,66)] py-10 xs:px-10 2xs-custom:px-8 2xs:px-6 px-4'>
             <div className='flex flex-col items-center'>
                 {/* Logo */}
                 <h1 className="text-4xl font-bold text-white font-heading flex gap-x-2 items-center">
@@ -10,7 +10,7 @@ const Footer = () => {
                     Qubix
                 </h1>
                 {/* Nav Links */}
-                <ul className="mt-7 font-bold text-[15px] flex gap-x-8 font-body text-white">
+                <ul className="mt-7 font-bold text-[15px] flex xs:gap-x-8 2xs-custom:gap-x-6 gap-x-5 font-body text-white">
                     <li>Services.</li>
                     <li>Work.</li>
                     <li>Skills.</li>
